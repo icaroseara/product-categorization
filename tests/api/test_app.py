e9c64834bf60cd9d51ab9b696b1b3ea49e702c9d
Icaro Seara <ikro89@gmail.com>
2018-03-05 23:48:29 -0300

    Increase code coverage to app tests

--- tests/api/test_app.py
+++ tests/api/test_app.py
@@ -1,3 +1,4 @@
+# encoding=utf8
 import json
 import unittest
 
@@ -22,3 +23,22 @@ class AppTests(unittest.TestCase):
                               data=given_product,
                               content_type='application/json')
             assert response.status_code == 422
+
+    def test_categorize_product_with_valid_input(self):
+        description = u"""
+        Indicada para cães adultos de raça pequena, Ração Sabor Frango,
+        Contem apenas ingredientes nobres e selecionados sob rigoroso controle de qualidade,
+        Pelagem bonita e saudável, rico em acido graxo essenciais, Omega 3 e Omega 6,
+        Ajuda no equilibrio intestinal, combinação de ingredientes de alta digestibilidade,
+        fibras alimentares e prebioticos, Enriquecido com vitaminas e minerais que proporcional
+        maior saúde e vitalidade
+        """
+        given_product = {
+            "name": u"Ração Premier Pet Formula Cães Adultos Raças Pequenas",
+            "description": description
+        }
+        with self.api as c:
+            response = c.post(self.url,
+                              data=json.dumps(given_product),
+                              content_type='application/json')
+            assert response.status_code == 200
